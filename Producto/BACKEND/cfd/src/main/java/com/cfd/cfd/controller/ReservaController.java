fdb7830ccb7af9cb5efc3b682fb861c8e3fdf418
fernando <fe.zaratem@duocuc.cl>
2026-05-28 13:52:34 -0400

    Arreglo de codigo reservacontroller

--- Producto/BACKEND/cfd/src/main/java/com/cfd/cfd/controller/ReservaController.java
+++ Producto/BACKEND/cfd/src/main/java/com/cfd/cfd/controller/ReservaController.java
@@ -13,13 +13,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/reservas")
-@CrossOrigin(origins = "*") // Permite que el frontend de React se conecte sin error de CORS
+@CrossOrigin(origins = "*") 
 public class ReservaController {
 
     @Autowired
     private ReservaService reservaService;
 
-    // 1. OBTENER TODAS LAS RESERVAS (◄--- NUEVO ENDPOINT CRÍTICO AGREGADO PARA EL DASHBOARD ADMIN ---)
+    // OBTENER TODAS LAS RESERVAS 
     @GetMapping
     public ResponseEntity<List<Reserva>> listarTodasLasReservas() {
         // Busca todas las reservas almacenadas en la base de datos MySQL de forma general
@@ -27,25 +27,25 @@ public class ReservaController {
         return ResponseEntity.ok(reservas);
     }
 
-    // 2. CREAR RESERVA (Público y Clientes)
+    //  CREAR RESERVA 
     @PostMapping
     public ResponseEntity<?> crearReserva(@RequestBody ReservaRequestDTO dto) {
         try {
             Reserva reservaConfirmada = reservaService.procesarNuevaReserva(dto);
-            return new ResponseEntity<>(reservaConfirmada, HttpStatus.CREATED); // 201 Created
+            return new ResponseEntity<>(reservaConfirmada, HttpStatus.CREATED);
         } catch (RuntimeException e) {
             // Si no hay cupo o falla algo -> 400 Bad Request
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
-    // 3. LISTAR HISTORIAL ESPECÍFICO DE UN USUARIO
+    //. LISTAR HISTORIAL ESPECÍFICO DE UN USUARIO
     @GetMapping("/usuario/{id}")
     public List<Reserva> listarPorUsuario(@PathVariable Integer id) {
         return reservaService.obtenerPorUsuario(id);
     }
 
-    // 4. CANCELAR AGENDAMIENTO
+    //  CANCELAR AGENDAMIENTO
     @DeleteMapping("/{id}/cancelar") // 
     public ResponseEntity<?> cancelarReserva(@PathVariable Integer id) {
         reservaService.cancelarReserva(id); // Llama al método que limpia y descuenta el cupo
